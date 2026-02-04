d38971f71b2e65cc10de4090d6f7b0ab3149b746
Catherine Li <licatherine0531@gmail.com>
2026-02-03 22:05:57 -0600

    drafting rtos system'

--- main/main.c
+++ main/main.c
@@ -3,40 +3,29 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define LED1 GPIO_NUM_2
-#define LED2 GPIO_NUM_4
+#define LED_PIN 2
 
-// ----- Task 1: Blink LED1 every 500 ms -----
-void blink_task_1(void *pvParameters)
+void light_on(void *pvParameters)
 {
-    /*gpio_reset_pin(LED1);
-    gpio_set_direction(LED1, GPIO_MODE_OUTPUT);*/
 
     while (1)
     {
-        /*gpio_set_level(LED1, 1);                     // Turn LED ON
-        vTaskDelay(pdMS_TO_TICKS(500));              // Wait 500 ms
-        gpio_set_level(LED1, 0);                     // Turn LED OFF
-        vTaskDelay(pdMS_TO_TICKS(500)); */             // Wait 500 ms
+        gpio_set_level(LED_PIN, 1);                    
+        vTaskDelay(2000 / portTICK_PERIOD_MS);                           
 
         printf("\n");
     }
 }
 
-// ----- Task 2: Blink LED2 every 200 ms -----
-void print(void *pvParameters)
+void light_off(void *pvParameters)
 {
-    /*gpio_reset_pin(LED2);
-    gpio_set_direction(LED2, GPIO_MODE_OUTPUT);*/
 
     while (1)
     {
-        /*gpio_set_level(LED2, 1);                     // Turn LED ON
-        vTaskDelay(pdMS_TO_TICKS(200));              // Wait 200 ms
-        gpio_set_level(LED2, 0);                     // Turn LED OFF
-        vTaskDelay(pdMS_TO_TICKS(200));              // Wait 200 ms*/
+        gpio_set_level(LED_PIN, 0);                    
+        vTaskDelay(1000 / portTICK_PERIOD_MS);                           
 
-        printf("*");
+        printf("\n");
     }
 }
 
@@ -45,7 +34,11 @@ void app_main(void)
 {
     printf("Starting FreeRTOS tasks...\n");
 
-    // Create both tasks
-    xTaskCreate(blink_task_1, "Blink1", 2048, NULL, 1, NULL);
-    //xTaskCreate(blink_task_2, "Blink2", 2048, NULL, 1, NULL);
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    xTaskCreate(light_on, "on", 2048, NULL, 1, NULL);
+    xTaskCreate(light_off, "off", 2048, NULL, 1, NULL);
+    
 }
+
